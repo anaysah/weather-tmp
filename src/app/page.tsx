80add3a54c4c029f3af773d48b451b7191b392a6
anaysah <ajaysahu0606@gmail.com>
2024-05-30 07:25:35 +0530

    select time and data to get the wheather data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,85 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+
+const API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+async function fetchData() {
+	try {
+		const res = await fetch(API_URL);
+		if (!res.ok) {
+			throw new Error("Failed to fetch data");
+		}
+		const data = await res.json();
+		console.log(data);
+		return data.hourly;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// const wheather_data = fetchData().then((data)=>data)
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [data, setData] = useState({ time: [], temperature_2m: [], relative_humidity_2m: [], wind_speed_10m: [] });
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const [uniqueDates, setUniqueDates] = useState([]);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const [selectedDate, setSelectedDate] = useState('');
+	const [selectedTime, setSelectedTime] = useState('00:00');
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const [wind, setWind] = useState('')
+	const [humidity, setHumidity] = useState('')
+	const [temperature, setTemperature] = useState('')
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+	useEffect(() => {
+		fetchData().then(setData).catch(console.error);
+	}, []);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	useEffect(() => {
+		console.log(data);
+
+		if (data.time.length > 0) {
+			const dates = data.time.map(dateTime => dateTime.split('T')[0]);
+			const uniqueDates = Array.from(new Set(dates));
+			setUniqueDates(uniqueDates);
+			setSelectedDate(uniqueDates[0])
+		}
+	}, [data]);
+
+	useEffect(()=>{
+		const time = `${selectedDate}T${selectedTime}`
+		const index = data.time.indexOf(time);
+		setWind(data.wind_speed_10m[index])
+		setHumidity(data.relative_humidity_2m[index])
+		setTemperature(data.temperature_2m[index])
+	},[selectedDate, selectedTime])
+
+	return (
+		<>
+			<div className="text-xl text-center">Weather API</div>
+			<div>
+				<span>Choose Date: </span>
+				<select value={selectedDate} onChange={(e)=>setSelectedDate(e.target.value)}>
+					{uniqueDates.map((value, index) => (
+						<option key={index} value={value}>{value}</option>
+					))}
+				</select>
+			</div>
+			<div>
+				<span>Choose Time: </span>
+				<select value={selectedTime} onChange={(e)=>setSelectedTime(e.target.value)}>
+					{
+						Array.from({ length: 24 }, (_, i) => `${String(i).padStart(2, '0')}:00`).map((value, index)=>(
+							<option key={index} value={value}>{value}</option>
+						))
+					}
+				</select>
+			</div>
+			<div>
+				<div>Wind: {wind}</div>
+				<div>Temperature: {temperature}</div>
+				<div>Humidity: {humidity}</div>
+			</div>
+		</>
+	); 
 }
---
+++ src/components/Weather.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Weather = () => {
+    const [data, setData] = useState()
+
+    const [wind, setWind] = useState("");
+    const [temperature_2m, setTemperature_2m] = useState("");
+    const [humidity, setHumidity] = useState("");
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Weather
